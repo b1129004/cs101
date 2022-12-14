a8b9d1e3d9debd6bfaa9e1edcf5268a2e4577629
b1129004 <120005659+b1129004@users.noreply.github.com>
2022-12-14 15:57:15 +0800

    Add files via upload

--- qz1/main.c
+++ qz1/main.c
@@ -1,1 +1,20 @@
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+
+    return 0;
+}
